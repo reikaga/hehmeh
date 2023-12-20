8b9b39b3fefc79bef577efe13b3f59744c266225
reikaga <151878642+reikaga@users.noreply.github.com>
2023-12-21 00:41:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
                   <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
               </svg>
-              Дз 1 еще в процессе, пожалуйста подождите) 
+              это что сайт? 
           </div>
           <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         `;
@@ -76,7 +76,7 @@ document.addEventListener('DOMContentLoaded', function() {
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
                 <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
             </svg>
-            Дз 2 еще в процессе, пожалуйста подождите) 
+            ну там короче форма какая то, да... 
         </div>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     `;
@@ -98,7 +98,7 @@ function showAlertWarning2() {
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
                 <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
             </svg>
-            Дз 3 еще в процессе, пожалуйста подождите) 
+            это че, новости? 
         </div>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     `;
@@ -111,6 +111,27 @@ function showAlertWarning2() {
         alertElement.remove();
     }, 2000);
 }
+  function showAlertWarning3() {
+    var alertElement = document.createElement("div");
+    alertElement.classList.add("alert", "alert-warning", "position-fixed", "top-0", "end-0", "m-4", "d-flex", "align-items-center", "justify-content-center", "text-center", "user-select-none");
+    alertElement.innerHTML = `
+        <div class="w-100">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+            </svg>
+            аэаэ, это менеджер) 
+        </div>
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      `;
+
+    alertElement.style.height = "40px";
+
+    document.body.appendChild(alertElement);
+
+    setTimeout(function() {
+        alertElement.remove();
+    }, 2000);
+  }
 
   var hw1Header = document.getElementById("hw1_head");
   hw1Header.addEventListener("click", function() {
@@ -131,6 +152,10 @@ function showAlertWarning2() {
   hw3Header.addEventListener("click", function() {
       showAlertWarning2();
   });
+   var hw4Header = document.getElementById("hw4_head");
+  hw4Header.addEventListener("click", function() {
+      showAlertWarning3();
+  });
 
 // Получите ссылку на кнопку Homework_2
 const homework2Button = document.getElementById("hw2_head");
@@ -147,6 +172,12 @@ const homework3Button = document.getElementById("hw3_head");
 homework3Button.addEventListener("click", function () {
     // Перенаправьте пользователя на страницу авторизации
     window.location.href = "scroll.html";
+});
+const homework4Button = document.getElementById("hw4_head");
+// Добавьте обработчик события клика на кнопке Homework_2
+homework4Button.addEventListener("click", function () {
+    // Перенаправьте пользователя на страницу авторизации
+    window.location.href = "manager1.html";
 });
 });//
 // Добавьте логику для страницы авторизации и перенаправления на "Вы успешно авторизовались"
