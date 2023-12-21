b074866f5af7dd1fce3583ab7f84cb34b10227b8
reikaga <151878642+reikaga@users.noreply.github.com>
2023-12-21 03:56:02 +0300

    Update manager.js

--- manager.js
+++ manager.js
@@ -1,7 +1,7 @@
 let orders = JSON.parse(localStorage.getItem('orders')) || [
-    { id: 1, cardNumber: '8479', orderNumber: '01', amount: 1200, customerName: 'Арк', status: 'В обработке', comment: 'лечу биполярку' },
-    { id: 2, cardNumber: '8791', orderNumber: '02', amount: 150, customerName: 'Кор', status: 'В обработке', comment: 'доедалус положите' },
-    { id: 3, cardNumber: '5597', orderNumber: '03', amount: 250, customerName: 'Опорник Б плента', status: 'В обработке', comment: 'где все' },
+    { id: 1, cardNumber: '8479', orderNumber: '01', amount: 3000, customerName: 'Арк', status: 'В обработке', comment: 'лечу биполярку' },
+    { id: 2, cardNumber: '8791', orderNumber: '02', amount: 2300, customerName: 'Кор', status: 'В обработке', comment: 'доедалус положите' },
+    { id: 3, cardNumber: '5597', orderNumber: '03', amount: 20, customerName: 'Опорник Б плента', status: 'В обработке', comment: 'где все' },
 ];
 let deletedOrders = JSON.parse(localStorage.getItem('deletedOrders')) || [];
 let originalOrders = orders.slice();
